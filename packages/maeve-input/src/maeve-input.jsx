c4331d29a38e47326b556d6bf6aa34d67f0be6e8
ateev <ateev@live.com>
2017-01-11 18:55:23 +0530

    updaing input package

--- packages/maeve-input/src/maeve-input.jsx
+++ packages/maeve-input/src/maeve-input.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
 export default class MaeveInput extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      value: event.target.value,
+    });
+
+    if ( typeof this.props.source !== undefined ) {
+      console.log(this.props.source);
+    }
+
+  }
+
   render() {
     return (
       <div className="maeve-input">
-        <input type="text" name="maeve" />
+        <input
+          type="text"
+          name="maeve"
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
