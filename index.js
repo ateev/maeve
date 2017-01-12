f219a14d36644139c4f92ad95868a84e22063b62
ateev <ateev@live.com>
2017-01-12 17:40:48 +0530

    added index for maeve

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MaeveInput: require('./packages/maeve-input/dist/maeve-input.js'),
+  MaeveMulti: require('./packages/maeve-multi/dist/maeve-multi.js'),
+  MaeveDropdown: require('./packages/maeve-dropdown/dist/maeve-dropdown.js'),
+};
