7f496d4981272a4093863b44ae1c7939383534ad
ateev <ateev@live.com>
2017-01-15 14:25:24 +0530

    removed multi from app.js

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import MaeveInput from 'maeve-input';
-import MaeveMulti from 'maeve-multi';
 import './App.css';
 
 class App extends Component {
-  filterResults = (item, query) => item.toLowerCase().includes(query.toLowerCase())
 
-  asyncFunction = (value) => {
-    return ["apple", "banana", "cat", "appollo", "caterpillar"]
-    .filter(
-      item => this.filterResults(item, value)
-    );
-  }
+  filterResults = (item, query) => item.toLowerCase().includes(query.toLowerCase())
 
   addNewItem = (value) => {
     console.log("adding new option " + value);
   }
 
   onValueUpdate = (value) => {
-    console.log("Value updated");
+    console.log("Value updated to " + value);
+  }
+
+  asyncFunction = (value) => {
+    return ["apple", "banana", "cat", "appollo", "caterpillar"]
+    .filter(
+      item => this.filterResults(item, value)
+    );
   }
 
   render() {
@@ -42,16 +42,14 @@ class App extends Component {
         />
         <br />
         <div> <h2>Maeve-input with source as a function and a label</h2> </div>
-        <MaeveMulti>
-          <MaeveInput
-            // Essentials
-            id="asyncAutoComplete"
-            onValueUpdate={this.onValueUpdate}
-            // Optionals
-            autocomplete={{ source: this.asyncFunction }}
-            label="Searchable Items"
-          />
-        </MaeveMulti>
+        <MaeveInput
+          // Essentials
+          id="asyncAutoComplete"
+          onValueUpdate={this.onValueUpdate}
+          // Optionals
+          autocomplete={{ source: this.asyncFunction }}
+          label="Searchable Items"
+        />
       </div>
     );
   }
