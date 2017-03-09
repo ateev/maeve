bc2ec7fbe85584cb3d4cb35691bc151721228282
ateev <ateev@live.com>
2017-03-09 16:12:27 +0530

    added base component

---
+++ packages/maeve-toggle/src/maeve-toggle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class MaeveToggle extends React.Component {
+  render() {
+    return (
+      <div className="maeve-toggle">
+        <input type="text" name="maeve" />
+      </div>
+    );
+  }
+};
