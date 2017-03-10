f8edea721b1e8c626d7668bab5f74f1c91520d9a
ateev <ateev@live.com>
2017-03-10 13:05:31 +0530

    added condition for addeding label in toggle

--- packages/maeve-toggle/src/maeve-toggle.jsx
+++ packages/maeve-toggle/src/maeve-toggle.jsx
@@ -4,8 +4,14 @@ export default class MaeveToggle extends React.Component {
   render() {
     return (
       <div className="maeve-toggle">
+        { typeof this.props.label !== undefined ?
+          <label>{this.props.label}</label>
+          :
+          ''
+        }
         <input
           id={this.props.id}
+          name={this.props.id}
           className="toggle-input"
           type="checkbox"
         />
