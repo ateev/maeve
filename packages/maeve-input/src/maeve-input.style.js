06326beec42e29a0ea11c1116769bd1331e8de7c
ateev <ateev@live.com>
2017-03-14 13:28:02 +0530

    added styled components for styling

---
+++ packages/maeve-input/src/maeve-input.style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const InputLabel = styled.label`
+  display: inline-block;
+  width: 100%;
+  margin-bottom: 5px;
+  font-size: 16px;
+`;
+
+export const InputField = styled.input`
+  width: 358px;
+  height: 32px;
+  border-radius: 2px;
+  border: solid 1px #cccccc;
+`;
